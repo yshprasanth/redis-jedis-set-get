aa7a4d219185f6892158da92c79ce08cf8a03999
Yalamanchili Sri Hari Prasanth <yshprasanth@gmail.com>
2021-03-30 09:57:08 +0100

    initial commit

---
+++ src/main/java/com/ssscl/redis/jedis/App.java
@@ -0,0 +1,13 @@
+package com.ssscl.redis.jedis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
